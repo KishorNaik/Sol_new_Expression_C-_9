cdeafdab6718acd2ea30f3bd29b505d639574822
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-13 18:30:51 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sol_Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person person = new()
+            {
+                FirstName = "Kishor",
+                LastName = "Naik"
+            };
+
+            Display(person: new()
+            {
+                FirstName = "Eshaan",
+                LastName = "Naik"
+            });
+        }
+
+        private static void Display(Person person)
+        {
+            Console.WriteLine($"{person.FirstName} {person.LastName}");
+        }
+    }
+
+    public class Person
+    {
+        public String FirstName { get; set; }
+
+        public String LastName { get; set; }
+    }
+}
